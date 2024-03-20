7e4d9cda0434723d4d9b220cf7305ae8b7b2262a
2k <104735018+justcallme2k@users.noreply.github.com>
2024-03-20 20:36:06 +0000

    rem: theme

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -1,17 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-import { ThemeProvider } from "@mui/material/styles"
-import { theme } from "../theme";
-
 import { Provider } from "./Provider/Provider";
 
 import "./main.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <Provider />
-        </ThemeProvider>
+        <Provider />
     </React.StrictMode>,
 )
