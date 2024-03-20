371bc17f96c27d4cd5ce645c99bf594c9c40987a
2k <104735018+justcallme2k@users.noreply.github.com>
2024-03-20 19:42:40 +0000

    Packages and close callback

--- backend/client/modules/Callbacks.ts
+++ backend/client/modules/Callbacks.ts
@@ -1,6 +1,6 @@
 import { NUI } from "./NUI"
 
-RegisterNuiCallback("2k:close", function() {
+RegisterNuiCallback("Close", function() {
     NUI.SendAppEvent("setVisible", false)
     SetNuiFocus(false, false)
 })
